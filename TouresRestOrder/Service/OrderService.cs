cfa22012b9da8e83782e312a906d30b0c6322fc5
heysson <38928961+heysson@users.noreply.github.com>
2018-09-03 23:14:46 -0500

    Insertar orden
    Insertar orden

--- TouresRestOrder/Service/OrderService.cs
+++ TouresRestOrder/Service/OrderService.cs
@@ -23,28 +23,26 @@ namespace TouresRestOrder.Service
             IRepository<OracleParameterCollection> repository = new OracleRepository(connString, "P_ORDID");
             var response = new ResponseBase<Boolean>();
 
-            repository.Parameters.Add("P_Comments", OracleDbType.Int64).Value = data.Comments;//data.UserName;
-            repository.Parameters.Add("P_OrdenDate", OracleDbType.Varchar2, 200).Value = data.OrdenDate;
-            repository.Parameters.Add("P_CustId", OracleDbType.Int64).Value = data.CustId;//data.UserName;
-            repository.Parameters.Add("P_Price", OracleDbType.Varchar2, 200).Value = data.Price;
-            repository.Parameters.Add("P_Status", OracleDbType.Int64).Value = data.Status;//data.UserName;
-            repository.Parameters.Add("P_ORDID", OracleDbType.Long).Direction = ParameterDirection.Output;
+            repository.Parameters.Add("P_CustId", OracleDbType.Int64).Value = data.CustId;
+            repository.Parameters.Add("P_OrdenDate", OracleDbType.Date, 200).Value = DateTime.Now;          
+            repository.Parameters.Add("P_Price", OracleDbType.Long, 200).Value = data.Price;
+            repository.Parameters.Add("P_Status", OracleDbType.Varchar2).Value = data.Status;
+            repository.Parameters.Add("P_Comments", OracleDbType.Varchar2).Value = data.Comments;
+            repository.Parameters.Add("P_ORDID", OracleDbType.Int64).Direction = ParameterDirection.Output;
 
-            var result = repository.Get("PKG_B2C_ORDERS.B2C_ORDERS_INSERT");
+            repository.SaveChanges("PKG_B2C_ORDERS.B2C_ORDERS_INSERT");
             if (repository.Status.Code == Status.Ok)
             {
-                foreach (var item in result)
-                {
-                  
-                }
                 response.Data = true;
+                response.Message = repository.Status.Message;
             }
             else
             {
+                response.Data = false;
                 response.Message = repository.Status.Message;
             }
             response.Code = repository.Status.Code;
-            response.Data = false;
+            
             return await Task.Run(() => response);
         }
     }
