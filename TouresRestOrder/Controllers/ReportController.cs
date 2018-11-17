907b809ba19bbc0e61527131e975ffe2c2110b0e
heysson <38928961+heysson@users.noreply.github.com>
2018-11-17 14:15:17 -0500

    oms
    oms

---
+++ TouresRestOrder/Controllers/ReportController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TouresCommon;
+using TouresRestOrder.Model;
+using TouresRestOrder.Service;
+
+namespace TouresRestOrder.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/v1/Report")]
+    public class ReportController : Controller
+    {
+        private IConfiguration config;
+        private string oracleConn;
+
+        public ReportController(IConfiguration configuration)
+        {
+            config = configuration;
+            oracleConn = config["oracleConnection"];
+        }
+
+        /// <summary>
+        /// Obtiene las ordenes de un usuario por id
+        /// </summary>
+        /// <param name="customer">Id de usuario</param>
+        /// <returns>Devuelve un objeto con la información de las ordenes consultadas</returns>
+        /// <response code="422">Invalid Data</response>
+        [ProducesResponseType(200)]
+        [ProducesResponseType(422)]
+        [HttpGet("{tipo}")]
+        public async Task<IActionResult> GetOrders(int tipo)
+        {
+            var result = new ResponseBase<List<ReportOrdenModel>>();
+
+            result = await new ReportService(oracleConn).GetReportOrders(tipo);
+
+            return this.Result(result.Code, result);
+        }
+
+    }
+}
---
+++ TouresRestOrder/Service/ReportService.cs
@@ -0,0 +1,68 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+using TouresCommon;
+using TouresCommon.Middleware;
+using TouresRepository;
+using TouresRestOrder.Model;
+
+namespace TouresRestOrder.Service
+{
+    public class ReportService
+    {
+        private string connString = "";
+        public ReportService(string ConnectionString)
+        {
+            connString = ConnectionString;
+        }
+
+        public async Task<ResponseBase<List<ReportOrdenModel>>> GetReportOrders(int tipobusqueda)
+        {
+            var response = new ResponseBase<List<ReportOrdenModel>>();
+
+            if (tipobusqueda > 0)
+            {
+                IRepository<OracleParameterCollection> repository = new OracleRepository(connString, "C_DATASET");
+                var order = new ReportOrdenModel();
+                var lOrder = new List<ReportOrdenModel>();
+
+                repository.Parameters.Add("P_TIPO_INFORME", OracleDbType.Int32).Value = tipobusqueda;
+                repository.Parameters.Add("P_FECHA1", OracleDbType.Date).Value = DateTime.Now;
+                repository.Parameters.Add("P_FECHA2", OracleDbType.Date).Value = DateTime.Now;
+                repository.Parameters.Add("C_DATASET", OracleDbType.RefCursor).Direction = ParameterDirection.Output;
+
+                var result = repository.Get("PKG_B2C_REPORT.B2C_ORDERS_SELECT");
+                if (repository.Status.Code == Status.Ok)
+                {
+                    foreach (var item in result)
+                    {
+                        order = new ReportOrdenModel();
+                        order.ordid = long.Parse(item["ORDID"].ToString());
+                        order.comments = item["COMMENTS"].ToString();
+                        order.fname = item["FNAME"].ToString();
+                        order.ordendate = DateTime.Parse(item["ORDENDATE"].ToString());
+                        order.nombre_estado = item["NOMBRE_ESTADO"].ToString();
+                        order.price =  double.Parse(item["PRICE"].ToString());
+                        lOrder.Add(order);
+                    }
+
+                    response.Data = lOrder;
+                }
+                else
+                {
+                    response.Message = repository.Status.Message;
+                }
+                response.Code = repository.Status.Code;
+            }
+            else
+            {
+                response.Code = Status.InvalidData;
+                response.Message = "The field CustId is zero(0)";
+            }
+
+            return await Task.Run(() => response);
+        }
+    }
+}
